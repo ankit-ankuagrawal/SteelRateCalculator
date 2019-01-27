7fa21d584529a67b217c1a3ea9d8d4e09fec3300
ankit-ankuagrawal <ankit.ankuagrawal@gmail.com>
2019-01-27 06:58:06 +0530

    New Version 1.1, following are the updates 1) new cal added for rate difference 2) new cal added for other expenses 3) added clear button to clear the edit text

--- app/src/main/java/com/sample/app/steelratecal/MainActivity.java
+++ app/src/main/java/com/sample/app/steelratecal/MainActivity.java
@@ -3,26 +3,28 @@ package com.sample.app.steelratecal;
 
 import android.os.Bundle;
 import android.text.Editable;
-import android.text.TextUtils;
 import android.text.TextWatcher;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.text.DecimalFormat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity implements TextWatcher {
+public class MainActivity extends AppCompatActivity implements TextWatcher, View.OnTouchListener {
 
     private static final float FOURTEEN_RATE_OF_INTEREST = 14.0f;
     private static final float EIGHTEEN_RATE_OF_INTEREST = 18.0f;
 
-    private EditText etBasicPrice;
+    private EditText etBasicPrice, etRateDifference, etTransport;
     private TextView tv14, tv14Total;
     private TextView tv18, tv18Total;
-    private double basicPrice;
+    private double basicPrice, rateDifference, transport;
     private double interest14;
     private double interest18;
     private double total14;
@@ -34,7 +36,17 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
         setContentView(R.layout.activity_main);
 
         etBasicPrice = (TextInputEditText) findViewById(R.id.etBasicPrice);
+        etRateDifference = (TextInputEditText) findViewById(R.id.etRateDifference);
+        etTransport = (TextInputEditText) findViewById(R.id.etTransport);
+
         etBasicPrice.addTextChangedListener(this);
+        etRateDifference.addTextChangedListener(this);
+        etTransport.addTextChangedListener(this);
+
+        etBasicPrice.setOnTouchListener(this);
+        etRateDifference.setOnTouchListener(this);
+        etTransport.setOnTouchListener(this);
+
         tv14 = (TextView) findViewById(R.id.tv14);
         tv14Total = (TextView) findViewById(R.id.tv14Total);
         tv18 = (TextView) findViewById(R.id.tv18);
@@ -50,20 +62,21 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         try {
-            basicPrice = 0;
-            if (!TextUtils.isEmpty(s)) {
-                basicPrice = Double.valueOf(s.toString());
-            }
 
-            interest14 = calInterest(basicPrice, FOURTEEN_RATE_OF_INTEREST);
-            interest18 = calInterest(basicPrice, EIGHTEEN_RATE_OF_INTEREST);
-            total14 = calTotal(basicPrice, interest14);
-            total18 = calTotal(basicPrice, interest18);
+            basicPrice = Double.valueOf("0" + etBasicPrice.getText().toString());
+            rateDifference = Double.valueOf("0" + etRateDifference.getText().toString());
+            transport = Double.valueOf("0" + etTransport.getText().toString());
+
+            interest14 = calInterest(basicPrice + rateDifference, FOURTEEN_RATE_OF_INTEREST);
+            interest18 = calInterest(basicPrice + rateDifference, EIGHTEEN_RATE_OF_INTEREST);
+
+            total14 = calTotal(basicPrice + rateDifference + transport, interest14);
+            total18 = calTotal(basicPrice + rateDifference + transport, interest18);
+
             tv14.setText(formatUpto2Decimal(interest14));
             tv14Total.setText(formatUpto2Decimal(total14));
             tv18.setText(formatUpto2Decimal(interest18));
             tv18Total.setText(formatUpto2Decimal(total18));
-
 
         } catch (NumberFormatException e) {
 
@@ -85,5 +98,23 @@ public class MainActivity extends AppCompatActivity implements TextWatcher {
 
     private double calTotal(double basicPrice, double calculatedInterest) {
         return basicPrice + calculatedInterest;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        final int DRAWABLE_LEFT = 0;
+        final int DRAWABLE_TOP = 1;
+        final int DRAWABLE_RIGHT = 2;
+        final int DRAWABLE_BOTTOM = 3;
+
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            if (event.getRawX() >= (v.getRight() - ((EditText) v).getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                // your action here
+                ((TextInputEditText) v).setText("");
+                return false;
+            }
+
+        }
+        return false;
     }
 }
